7318d5b45070d53f8dd1e59970736043246be5fe
dmytriim@wix.com <dmytriim@wix.com>
2020-02-23 11:25:16 +0200

    some commit

--- scala/build.sbt
+++ scala/build.sbt
@@ -5,3 +5,4 @@ version := "1.0"
 scalaVersion := "2.12.1"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1"
+libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.14.1" % "test"
--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -1,58 +1,77 @@
 package com.gildedrose
 
 class GildedRose(val items: Array[Item]) {
-
-
   def updateQuality() {
-    for (i <- 0 until items.length) {
-      if (!items(i).name.equals("Aged Brie")
-        && !items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        if (items(i).quality > 0) {
-          if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-            items(i).quality = items(i).quality - 1
-          }
-        }
-      } else {
-        if (items(i).quality < 50) {
-          items(i).quality = items(i).quality + 1
-
-          if (items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items(i).sellIn < 11) {
-              if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
-              }
-            }
-
-            if (items(i).sellIn < 6) {
-              if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
-              }
-            }
-          }
-        }
-      }
-
-      if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-        items(i).sellIn = items(i).sellIn - 1
-      }
-
-      if (items(i).sellIn < 0) {
-        if (!items(i).name.equals("Aged Brie")) {
-          if (!items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items(i).quality > 0) {
-              if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-                items(i).quality = items(i).quality - 1
-              }
-            }
-          } else {
-            items(i).quality = items(i).quality - items(i).quality
-          }
-        } else {
-          if (items(i).quality < 50) {
-            items(i).quality = items(i).quality + 1
-          }
-        }
-      }
+    for (i <- items.indices) {
+      items(i) = myMap(items(i).name)(items(i))
     }
+  }
+
+  type ItemUpdater = Item => Item
+  private val defaultLogic: ItemUpdater = defaultBehaviour(decreaseQuality(_))
+  private val agedBrieLogic: ItemUpdater = defaultBehaviour(increaseQuality(_))
+  private val passesLogic: ItemUpdater = qualityInBounds(sellInDecrease(handleBackstagePasses))
+  private val ragnarosLogic: ItemUpdater = (item: Item) => item
+
+  private def defaultBehaviour(itemUpdater: ItemUpdater): ItemUpdater =
+    conjuredDecreaseTwiceAsFast(
+      qualityInBounds(
+        sellInDecrease(
+          qualityTwiceAsFastAfterSellIn(itemUpdater))))
+
+  val myMap: Map[String, ItemUpdater] = Map(
+    "Aged Brie" -> agedBrieLogic,
+    "Backstage passes to a TAFKAL80ETC concert" -> passesLogic,
+    "Sulfuras, Hand of Ragnaros" -> ragnarosLogic
+  )
+    .withDefaultValue(defaultLogic)
+
+  private def qualityInBounds(itemUpdater: ItemUpdater): ItemUpdater = item => {
+    val newItem = itemUpdater(item)
+    Item(newItem.name, newItem.sellIn, newItem.quality min 50 max 0)
+  }
+
+  private def sellInOK(item: Item): Boolean = item.sellIn > 0
+  private def qualityTwiceAsFastAfterSellIn(itemUpdater: ItemUpdater): ItemUpdater = item => {
+    val newItem = itemUpdater(item)
+    if (sellInOK(item)) {
+      newItem
+    } else {
+      val qualityDifference = item.quality - newItem.quality
+      decreaseQuality(newItem, qualityDifference)
+    }
+  }
+
+  private def isConjured(item: Item): Boolean = item.name.toLowerCase.contains("conjured")
+  private def conjuredDecreaseTwiceAsFast(itemUpdater: ItemUpdater): ItemUpdater = item => {
+    val newItem = itemUpdater(item)
+    if (!isConjured(item)) {
+      newItem
+    } else {
+      val qualityDifference = item.quality - newItem.quality
+      decreaseQuality(newItem, qualityDifference)
+    }
+  }
+
+  private def sellInDecrease(itemUpdater: ItemUpdater): ItemUpdater = item => {
+    val newItem = itemUpdater(item)
+    Item(newItem.name, newItem.sellIn - 1, newItem.quality)
+  }
+
+  private def handleBackstagePasses(item: Item): Item = {
+    item.sellIn match {
+      case sellIn@_ if sellIn <= 0 => new Item(item.name, sellIn, 0)
+      case sellIn@_ if sellIn <= 5 => increaseQuality(item,3)
+      case sellIn@_ if sellIn <= 10 => increaseQuality(item,2)
+      case _ => increaseQuality(item)
+    }
+  }
+
+  private def decreaseQuality(item: Item, n: Int = 1): Item = {
+    Item(item.name, item.sellIn, item.quality - n)
+  }
+
+  private def increaseQuality(item: Item,n: Int = 1): Item = {
+    Item(item.name, item.sellIn, item.quality + n)
   }
 }
